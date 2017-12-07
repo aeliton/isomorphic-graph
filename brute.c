b4a39fd6a44455fc21ffb7f67a76839674ff007f
Aeliton G. Silva <aeliton@gmail.com>
2017-12-07 17:25:21 -0300

    Generate all solutions

--- brute.c
+++ brute.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define MAX 200
 
@@ -26,13 +27,49 @@ void print(char *g) {
     }
 }
 
+int verify() {
+    return 0;
+}
+
+int brute(char* symbols, char *map, int size, int available) {
+    // for each vertice in g2, try to map it to g1:0 
+    if (available == 0) {
+        printf("[%s] [%s]\n", symbols, map);
+        verify();
+        return 0;
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (symbols[i] != ' ') {
+            char sym = symbols[i];
+            symbols[i] = ' ';
+            map[size - available] = sym;
+            brute(symbols, map, size, available - 1);
+            symbols[i] = sym;
+        }
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     read((char*) &g1);
     read((char*) &g2);
-    print((char*) &g1);
-    print((char*) &g2);
+    /*print((char*) &g1);
+    print((char*) &g2);*/
 
+    char symbols[11];
+    char map[11];
+
+    memset(&symbols, 0, sizeof(symbols));
+    memset(&map, 0, sizeof(map));
+
+    for (int i = 0; i < 10; i++) {
+        symbols[i] = '0' + i;
+    }
+
+    brute(symbols, map, 10, 10);
 
     // print graphs function
     // generate signature for g1

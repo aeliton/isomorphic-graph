18b5d8af29ba3c552dc2dae8e04a6c4420ae0eaa
Aeliton G. Silva <aeliton@gmail.com>
2017-12-13 13:32:46 -0300

    Fix graph generation

--- gengraph.c
+++ gengraph.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 #include <time.h>
+#include <limits.h>
 
 #define USAGE fprintf(stderr, "Usage:\n\t %s -n NUM_VERTICES -e NUM_EDGES \
         NUM_GRAPHS\n", argv[0])
 
-int picky_rand(char limit, char ignore) {
+int
+picky_rand(char limit, char ignore) {
     int r = ignore;
     while (r == ignore) {
         r = rand() % limit;
@@ -15,13 +18,33 @@ int picky_rand(char limit, char ignore) {
 }
 
 char*
+picky_rand_set(char *set, char amount, char limit, char ignore) {
+    char used[CHAR_MAX];
+    int i, r;
+    memset(&used, 0, sizeof(used));
+
+    for (i = 0, r; i < amount; i++) {
+        r = rand() % limit;
+        while (used[r] || r == ignore) r = (r + 1) % limit;
+        set[i] = r;
+        used[r] = 1;
+    }
+    set[i] = -1;
+
+    return set;
+}
+
+char*
 generate(char *graph, int vertices, int isomorph)
 {
     graph[0] = vertices;
-    for (int i = 0, pos = 1; i < vertices; i++, pos += graph[pos]) {
+    for (int i = 0, pos = 1; i < vertices; i++, pos += graph[pos] + 1) {
         graph[pos] = picky_rand(vertices, 0);
-        for (int j = 1; j <= graph[pos]; j++) {
-            graph[pos+j] = picky_rand(vertices, j);
+        char set[CHAR_MAX];
+        picky_rand_set((char*) &set, graph[pos], vertices, i);
+
+        for (int j = 0; set[j] > -1; j++) {
+            graph[pos + j + 1] = set[j];
         }
     }
 }
@@ -29,7 +52,7 @@ generate(char *graph, int vertices, int isomorph)
 void
 print(char *graph) {
     int j;
-    for (int i = 0, pos = 1; i < graph[0]; i++, pos += graph[pos]) {
+    for (int i = 0, pos = 1; i < graph[0]; i++, pos += graph[pos] + 1) {
         for (j = 1; j < graph[pos]; j++) {
             printf("%d ", graph[pos + j]);
         }
@@ -73,6 +96,7 @@ main(int argc, char *argv[])
     srand(time(NULL));
 
     char graph[256];
+    memset(&graph, -1, sizeof(graph));
     generate((char*) &graph, n, i);
     print((char*) &graph);
 

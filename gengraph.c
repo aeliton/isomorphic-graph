1a0c4711f9a446bfd09f00c1885c8cd0e8b109d0
Aeliton G. Silva <aeliton@gmail.com>
2017-12-13 12:23:53 -0300

    Generate graphs (allows edge duplication)

--- gengraph.c
+++ gengraph.c
@@ -1,27 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <time.h>
 
 #define USAGE fprintf(stderr, "Usage:\n\t %s -n NUM_VERTICES -e NUM_EDGES \
         NUM_GRAPHS\n", argv[0])
 
-int main(int argc, char *argv[])
+int picky_rand(char limit, char ignore) {
+    int r = ignore;
+    while (r == ignore) {
+        r = rand() % limit;
+    }
+    return r;
+}
+
+char*
+generate(char *graph, int vertices, int isomorph)
 {
-    int n, e, num_g;
+    graph[0] = vertices;
+    for (int i = 0, pos = 1; i < vertices; i++, pos += graph[pos]) {
+        graph[pos] = picky_rand(vertices, 0);
+        for (int j = 1; j <= graph[pos]; j++) {
+            graph[pos+j] = picky_rand(vertices, j);
+        }
+    }
+}
+
+void
+print(char *graph) {
+    int j;
+    for (int i = 0, pos = 1; i < graph[0]; i++, pos += graph[pos]) {
+        for (j = 1; j < graph[pos]; j++) {
+            printf("%d ", graph[pos + j]);
+        }
+        printf("%d%c", graph[pos + j], (i == graph[0] - 1 ? '.' : ';'));
+    }
+}
+
+int
+main(int argc, char *argv[])
+{
+    int n, e, num_g, i;
     char c;
 
-    n = e = num_g = -1;
+    n = e = num_g = i = -1;
 
-    while ((c = getopt(argc, argv, "n:e:")) != -1) {
+    while ((c = getopt(argc, argv, "n:e:i")) != -1) {
         switch(c) {
             case 'n':
                 if (!sscanf(optarg, "%d", &n)) {
                     fprintf(stderr, "Invalid option for '%c': %s\n", c, optarg);
+                    opterr++;
                 }
                 break;
             case 'e':
                 if (!sscanf(optarg, "%d", &e)) {
                     fprintf(stderr, "Invalid option for '%c': %s\n", c, optarg);
+                    opterr++;
                 }
                 break;
             case '?':
@@ -30,9 +65,16 @@ int main(int argc, char *argv[])
         }
     }
 
-    if (!access(argv[optind], R_OK) || !sscanf(argv[optind], "%d", &num_g)) {
+    if (optind == argc || !sscanf(argv[optind], "%d", &num_g)) {
         USAGE;
+        exit(1);
     }
+
+    srand(time(NULL));
+
+    char graph[256];
+    generate((char*) &graph, n, i);
+    print((char*) &graph);
 
     return 0;
 }
